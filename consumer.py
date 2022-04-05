9bbd52a47bf92df4c307c917216e8c76cea2d830
dgarcia <dgarcia@acuant.com>
2022-04-04 20:37:18 -0500

    s3 update keep data

--- consumer.py
+++ consumer.py
@@ -2,6 +2,7 @@ import json
 import boto3   
 import os 
 from kafka import KafkaConsumer
+from datetime import date, datetime
 
 BUCKET_NAME = os.getenv('BUCKET_NAME')
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
@@ -11,11 +12,21 @@ KAFKA_BROKER = os.getenv('KAFKA_BROKER').split(',')
 consumer = KafkaConsumer ('demo-kafka-topic',bootstrap_servers = KAFKA_BROKER)
 
 s3 = boto3.resource('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
-s3object = s3.Object( BUCKET_NAME, 'now/test.json')
+#s3 = boto3.resource('s3', aws_access_key_id='AKIAVK3WWRUFTXGKLZKM', aws_secret_access_key='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')
+s3object = s3.Object( 
+    BUCKET_NAME,
+    '{today}/kafka_test_{now}.json'.format(
+        today=str(datetime.today()),
+        now=str(datetime.now())
+    )
+)
 
 for message in consumer:
     print(message)
-    print(message[6])
+    s3object.put(
+        #Body=(bytes(json.dumps(message[6]).encode('UTF-8')))
+        Body=(bytes(message[6]))
+    )
 
 # json_data = {
 # 'eventId': '7a378b69-673a-4fbd-935a-84c2af636f9d',
@@ -26,9 +37,7 @@ for message in consumer:
 # 'sourceState': {'3rdParty': 'LexisNexis', '3rdPartyService': 'Risk Classifier'}
 # }
 
-# s3object.put(
-#     Body=(bytes(json.dumps(json_data).encode('UTF-8')))
-# )
+
 
 
 '''
